3914f6982320c55d8398c0f09756ab0a6ca9b9d9
AlJawharhALOtaibi <ALJawharhALOtaibi@gmail.com>
2023-11-29 15:17:26 +0300

    Copy 2 *Last version*

--- foxus-MC2/MainPage.swift
+++ foxus-MC2/MainPage.swift
@@ -2,17 +2,7 @@
 //  MainPage.swift
 //  foxus-MC2
 //
-//  Created by leena alyami on 12/05/1445 AH.
+//  Created by AlJawharh AlOtaibi on 14/05/1445 AH.
 //
 
-import SwiftUI
-
-struct MainPage: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-    }
-}
-
-#Preview {
-    MainPage()
-}
+import Foundation
